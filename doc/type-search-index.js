4b6d08ec29bdc8ac8e2c185ab2b7d85501e9ab38
Oskar Karlsson <90701772+Karlzzon@users.noreply.github.com>
2022-12-13 16:21:58 +0100

    Delete doc directory

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"SudokuBoard"},{"p":"<Unnamed>","l":"SudokuSolver"},{"p":"<Unnamed>","l":"TestSudokuBoard"},{"p":"<Unnamed>","l":"UI"}];updateSearchResults();
